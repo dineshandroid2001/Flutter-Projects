d1620d10979bf00f6844e808e84c1c11bf683c19
Dinesh <dinesh.kumar@aspiresys.com>
2024-02-22 15:34:47 +0530

    change

--- my_practice/lib/main.dart
+++ my_practice/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_practice/home_page.dart';
+import 'package:my_practice/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: SplashScreen(),
     );
   }
 }
